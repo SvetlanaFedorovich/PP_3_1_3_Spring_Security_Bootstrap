d12e131a75d0e3fc27467c913a6c912132dca439
fedor <fedorovich-lana@mail.ru>
2022-02-19 14:27:03 +0300

    init commit

---
+++ src/main/java/security/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import security.model.User;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    Optional<User> findById(Long Id);
+}
---
+++ src/main/java/security/service/RoleService.java
@@ -0,0 +1,10 @@
+package security.service;
+
+
+import security.model.Role;
+
+import java.util.List;
+
+public interface RoleService {
+    List<Role> getAllRoles();
+}
